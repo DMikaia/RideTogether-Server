d491f67fc15c428fc8ef14c2c91105bec8705aff
DMikaia <faeldan03@gmail.com>
2024-07-28 22:09:04 +0300

    Setup the firebase, prisma and postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FirebaseModule } from './firebase/firebase.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ cache: true }), FirebaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
